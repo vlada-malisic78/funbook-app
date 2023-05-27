002ebbb86bbe8afc245c0151061f053be34ff728
vlada <malisicvlada1978@gmail.com>
2023-05-28 01:09:02 +0200

    Navigation

--- surfaces/Profile.js
+++ surfaces/Profile.js
@@ -1,15 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useHeaderHeight } from "@react-navigation/elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useHeaderHeight } from "@react-navigation/elements";
 
-export default function Feed() {
+export default function Profile() {
   const headerHeight = useHeaderHeight();
-
   return (
     <SafeAreaView style={{ flex: 1, paddingTop: headerHeight }}>
       <View>
-        <Text style={{ fontFamily: "Poppins_400Regular" }}>Feed</Text>
+        <Text>Profile</Text>
       </View>
     </SafeAreaView>
   );
